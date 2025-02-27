4067ea989858c788e293c479578f51a7a5403c89
Slava <slavamonster6@gmail.com>
2025-02-27 14:43:34 +0300

    add db and migrate, prettier

---
+++ prisma/migrations/20250226153214_init/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log("Game: ", games);
+
   return (
     <div>
       <Button size="lg">Hello</Button>
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
