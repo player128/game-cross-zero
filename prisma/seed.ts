61c95a02b2334ccd1ce2a1247ebcef0b940ba9cb
Slava <slavamonster6@gmail.com>
2025-03-26 17:39:11 +0300

    restruct model prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,44 +3,44 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      login: 'user',
-      passwordHash: 'sdfklsdjfl',
-      rating: 1000,
-    }
-  });
+  // const user = await prisma.user.create({
+  //   data: {
+  //     login: 'user',
+  //     passwordHash: 'sdfklsdjfl',
+  //     rating: 1000,
+  //   }
+  // });
 
-  const user2 = await prisma.user.create({
-    data: {
-      login: 'user2',
-      passwordHash: 'sdfklsdjfl',
-      rating: 800,
-    }
-  });
+  // const user2 = await prisma.user.create({
+  //   data: {
+  //     login: 'user2',
+  //     passwordHash: 'sdfklsdjfl',
+  //     rating: 800,
+  //   }
+  // });
 
-  await prisma.game.create({
-    data: {
-      field: Array(9).fill(null),
-      status: 'idle',
-      players: {
-        connect: {
-          id: user.id
-        }
-      }
-    },
-  });
-  await prisma.game.create({
-    data: {
-      field: Array(9).fill(null),
-      status: 'idle',
-      players: {
-        connect: {
-          id: user2.id
-        }
-      }
-    },
-  });
+  // await prisma.game.create({
+  //   data: {
+  //     field: Array(9).fill(null),
+  //     status: 'idle',
+  //     players: {
+  //       connect: {
+  //         id: user.id
+  //       }
+  //     }
+  //   },
+  // });
+  // await prisma.game.create({
+  //   data: {
+  //     field: Array(9).fill(null),
+  //     status: 'idle',
+  //     players: {
+  //       connect: {
+  //         id: user2.id
+  //       }
+  //     }
+  //   },
+  // });
 }
 main()
   .then(async () => {
--- src/entities/game/respositories/game.ts
+++ src/entities/game/respositories/game.ts
@@ -1,18 +1,19 @@
 import { prisma } from "@/shared/lib/db";
 import { GameEntity, GameIdleEntity, GameOverEntity, PlayerEntity } from '../domain';
-import { Game, User, Prisma } from "@prisma/client";
+import { Game, User, Prisma, GamePlayer } from "@prisma/client";
 import { z } from "zod";
-import { removePassword } from "@/shared/lib/password";
 import { GameId } from "@/kernel/ids";
+
+const gameInclude = {
+    winner: { include:{ user: true } },
+    players: { include:{ user: true } },
+};
 
 async function gamesList(where?: Prisma.GameWhereInput): Promise<GameEntity[]> {
     const games = await prisma.game.findMany({
         where,
-        include: {
-            winner: true,
-            players: true,
-        }
-    })
+        include: gameInclude,
+    });
 
     return games.map(dbGameToGameEntity);
 }
@@ -22,16 +23,14 @@ async function startGame(gameId: GameId, player: PlayerEntity) {
         where: {id: gameId},
         data: {
             players: {
-                connect: {
-                    id: player.id,
+                create: {
+                    index: 1,
+                    userId: player.id,
                 }
             },
             status: "inProgress",
         },
-        include: {
-            winner: true,
-            players: true,
-        }
+        include: gameInclude,
     });
 
     return dbGameToGameEntity(game);
@@ -40,10 +39,7 @@ async function startGame(gameId: GameId, player: PlayerEntity) {
 async function getGame(where?: Prisma.GameWhereInput) {
     const game = await prisma.game.findFirst({
         where,
-        include: {
-            winner: true,
-            players: true,
-        }
+        include: gameInclude,
     });
 
     if (game) {
@@ -60,15 +56,13 @@ async function createGame(game: GameIdleEntity): Promise<GameEntity> {
             id: game.id,
             field: game.field,
             players: {
-                connect:{
-                    id: game.creator.id,
+                create:{
+                    index: 0,
+                    userId: game.creator.id,
                 },
             },
         },
-        include: {
-            players: true,
-            winner:true,
-        }
+        include: gameInclude,
     })
 
     return dbGameToGameEntity(createdGame);
@@ -78,11 +72,11 @@ const fieldSchema = z.array(z.union([z.string(), z.null()]));
 
 function dbGameToGameEntity(
     game:  Game & { 
-        players: User[];
-        winner?: User | null;
+        players: Array<GamePlayer & {user: User}>;
+        winner?: GamePlayer & {user: User} | null;
     },
 ): GameEntity {
-    const players = game.players.map(removePassword);
+    const players = game.players.sort((a, b) => a.index - b.index).map(dbPlayerToPlayer);
     switch (game.status) {
         case 'idle': {
             const [creator] = players;
@@ -114,11 +108,18 @@ function dbGameToGameEntity(
                 players: players,
                 field: fieldSchema.parse(game.field),
                 status: game.status,
-                winner: removePassword(game.winner),
+                winner: dbPlayerToPlayer(game.winner),
             } satisfies GameOverEntity;
         }
-    }
-    
+    }   
+}
+
+export const dbPlayerToPlayer = (db:GamePlayer & {user: User}): PlayerEntity => {
+    return {
+        id: db.user.id,
+        login: db.user.login,
+        rating: db.user.rating,
+    };
 }
 
 export const gameRepository = { gamesList, createGame, getGame, startGame };
